698b1b8a9a23dec1ccfb579122495655c11903d7
manianbu5700 <manianbu5700@gmail.com>
2026-04-24 11:44:48 +0530

    file modified

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test } from '@playwright/test';
 
 test('Login facebook', async ({ page }) => {
-  await page.goto('https://www.snapdeal.com/');
+  await page.goto('https://www.flipkart.com/');
 
 });
 
