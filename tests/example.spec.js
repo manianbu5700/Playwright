af5cd89dd63781601f764648d8db1e84fd4eee11
manianbu5700 <153504739+manianbu5700@users.noreply.github.com>
2026-04-24 11:37:24 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test } from '@playwright/test';
 
 test('Login facebook', async ({ page }) => {
-  await page.goto('https://www.amazon.in/');
+  await page.goto('https://www.snapdeal.com/');
 
 });
 
